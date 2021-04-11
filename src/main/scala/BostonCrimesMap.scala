4369744ce3a6a04fd1fdc2dcf5d6fa3995de33c7
moofik <moofik12@gmail.com>
2021-04-11 19:40:05 +0700

    add final statistics

--- src/main/scala/BostonCrimesMap.scala
+++ src/main/scala/BostonCrimesMap.scala
@@ -8,6 +8,8 @@ import org.apache.spark.sql.functions.broadcast
 object BostonCrimesMap extends App {
   Logger.getLogger("org").setLevel(Level.OFF)
 
+  val (inputFile1, inputFile2, outputFile) = (args(0), args(1), args(2))
+
   val spark = SparkSession
     .builder()
     .config("spark.sql.autoBroadcastJoinThreshold", 0)
@@ -18,42 +20,58 @@ object BostonCrimesMap extends App {
     .read
     .option("header", "true")
     .option("inferSchema", "true")
-    .csv("/home/alexander/IdeaProjects/untitled/src/main/scala/crime.csv")
+    .csv(inputFile1)
 
   val offenseCodes = spark
     .read
     .option("header", "true")
     .option("inferSchema", "true")
-    .csv("/home/alexander/IdeaProjects/untitled/src/main/scala/offense_codes.csv")
+    .csv(inputFile2)
 
   val offenseCodesBroadcast = broadcast(offenseCodes)
-
-  val windowSpec = Window
-    .partitionBy("YEAR", "MONTH", "DISTRICT", "OFFENSE_CODE")
-    .orderBy("YEAR", "MONTH")
 
   def limitSize(n: Int, arrCol: Column): Column =
     array((0 until n).map(arrCol.getItem): _*)
 
   import spark.implicits._
 
-  val frequentCrimesDF = crimeFacts
-    .withColumn("max_count", count("OFFENSE_CODE").over(windowSpec))
-    .groupBy("YEAR", "MONTH", "DISTRICT", "OFFENSE_CODE")
+  val windowDistrictByAllTime = Window
+    .partitionBy("DISTRICT", "OFFENSE_CODE")
+    .orderBy("YEAR", "MONTH")
+
+  var frequentCrimesDF = crimeFacts
+    .withColumn("max_count", count("OFFENSE_CODE").over(windowDistrictByAllTime))
+    .groupBy("DISTRICT", "OFFENSE_CODE")
     .agg(max("max_count").as("FREQUENCY"))
-    .select($"YEAR", $"MONTH", $"DISTRICT", $"FREQUENCY", $"OFFENSE_CODE".as("CODE"))
+    .select($"DISTRICT", $"FREQUENCY", $"OFFENSE_CODE".as("CODE"))
     .join(offenseCodesBroadcast, Seq("CODE"))
     .distinct()
-    .orderBy($"YEAR".asc, $"MONTH".asc, $"DISTRICT".desc, $"FREQUENCY".desc)
-    .select($"YEAR", $"MONTH", $"DISTRICT", $"FREQUENCY", trim(substring_index($"NAME", "-", 1)).as("CRIME_NAME"))
+    .orderBy($"DISTRICT".desc, $"FREQUENCY".desc)
+    .select($"DISTRICT", $"FREQUENCY", trim(substring_index($"NAME", "-", 1)).as("CRIME_NAME"))
     .toDF()
 
-  frequentCrimesDF.show()
-
-  frequentCrimesDF
-    .groupBy("YEAR", "MONTH", "DISTRICT")
+  frequentCrimesDF = frequentCrimesDF
+    .groupBy("DISTRICT")
     .agg(collect_list("CRIME_NAME").as("frequent_crimes"))
-    .orderBy($"YEAR".asc, $"MONTH".asc, $"DISTRICT".desc)
-    .select($"YEAR", $"MONTH", $"DISTRICT", limitSize(3, $"frequent_crimes").as("frequent_crime_types"))
-    .show();
+    .orderBy($"DISTRICT".desc)
+    .select($"DISTRICT", limitSize(3, $"frequent_crimes").as("frequent_crime_types"))
+
+  crimeFacts
+    .groupBy($"DISTRICT")
+    .agg(
+      count("*").as("crimes_total"),
+      ceil(count("*") / 12).as("crimes_monthly"),
+      avg("Lat").as("lat"),
+      avg("Long").as("long")
+    )
+    .orderBy($"crimes_total".desc)
+    .join(frequentCrimesDF, Seq("DISTRICT"))
+    .distinct()
+    .write
+    .parquet(outputFile)
+
+  spark
+    .read
+    .parquet(outputFile)
+    .show(false)
 }
